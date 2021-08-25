3716ce1c98659f6c4ec4dfae350be6ddab751c0f
stakagi0606 <shogo.takagi@onespower.com>
2021-08-25 14:46:57 +0900

    initial commit

---
+++ src/Study/Lesson.java
@@ -0,0 +1,39 @@
+package Study;
+
+import java.util.Scanner;
+
+public class Lesson {
+	
+	public static void main(String[] args) {
+		
+		// System.inでキーボード入力を受け付ける
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("名前を入力して、エンターを押してください");
+		// 名前を入力
+		String name = scanner.next();
+		System.out.println("こんにちは"+ name + "さん、突然ですが問題です");
+		
+		
+		System.out.println("[1] 日本の首都はどこでしょうか？");
+		System.out.println("1：京都");
+		System.out.println("2：奈良");
+		System.out.println("3：東京");
+		System.out.println("答えを半角数字で入力し，エンターを押してください =>");
+		
+		
+		int answer = 0;
+		while(answer != 3){
+			// 答えを入力
+			answer = scanner.nextInt();
+			
+			if(answer == 3) {
+				System.out.println("正解です");
+				scanner.close();
+			}else {
+				System.out.println("今は違います");
+			}
+		}
+	}
+}
---
+++ src/Study/Test.java
@@ -0,0 +1,10 @@
+package Study;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// test
+		System.out.println("Hello world");
+	}
+
+}
